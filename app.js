b50b8eb68f00d23742fb6e053e95f4ec9e5f0b75
syedubaidshah <obaidshah0099@gmail.com>
2024-03-06 00:53:01 +0500

    complete

--- app.js
+++ app.js
@@ -17,16 +17,21 @@ const winPatterns = [
   [6, 7, 8],
 ];
 
+const resetGame = () => {
+  turnO = true;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
-    console.log("box was clicked");
     if (turnO) {
       //player O
       box.innerText = "O";
       turnO = false;
     } else {
       //player x
-      box.innerText = "x";
+      box.innerText = "X";
       turnO = true;
     }
     box.disabled = true;
@@ -35,9 +40,23 @@ boxes.forEach((box) => {
   });
 });
 
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
 const showWinner = (winner) => {
   msg.innerText = `Congratulation, winner is ${winner}`;
   msgContainer.classList.remove("hide");
+  disableBoxes();
 };
 
 const checkWinner = () => {
@@ -48,9 +67,11 @@ const checkWinner = () => {
 
     if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
       if (pos1Val === pos2Val && pos2Val === pos3Val) {
-        console.log("winner", pos1Val);
         showWinner(pos1Val);
       }
     }
   }
 };
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
